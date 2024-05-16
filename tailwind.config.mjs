c455543cc559cd373d71d1fcd9d67fe76459a37a
Shinya Fujino <shf0811@gmail.com>
2024-05-16 22:13:18 +0900

    tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        13: "repeat(13, minmax(0, 1fr))",
+      },
+      colors: {
+        blue: {
+          400: "#2589FE",
+          500: "#0070F3",
+          600: "#2F6FEB",
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        "100%": {
+          transform: "translateX(100%)",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
