4ea2dc8f782dd6e29c9900a8d82e762afcdbd3ec
Shinya Fujino <shf0811@gmail.com>
2024-05-18 02:21:53 +0900

    ch09

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import db from "@astrojs/db";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import simpleStackStream from "simple-stack-stream";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [db(), react(), tailwind()],
+  integrations: [db(), react(), simpleStackStream(), tailwind()],
 });
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -18,12 +18,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await db.select().from(Revenue);
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log("Data fetch completed after 3 seconds.");
 
     return data;
   } catch (error) {
