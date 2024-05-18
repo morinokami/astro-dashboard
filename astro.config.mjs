b6b05ee18bb85680fa9cd8345a36b4fd2385cba9
Shinya Fujino <shf0811@gmail.com>
2024-05-19 00:33:01 +0900

    astro build --remote

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import db from "@astrojs/db";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import simpleStackStream from "simple-stack-stream";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercel({
+    imageService: true,
+  }),
   integrations: [db(), react(), simpleStackStream(), tailwind()],
   experimental: {
     actions: true,
